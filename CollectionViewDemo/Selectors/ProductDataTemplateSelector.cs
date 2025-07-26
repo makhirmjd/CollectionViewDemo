856b42551dfe4b32b7f9fb31bf5c931365d612ee
Abdulmalik Muhammad <abdulmalik.m.m@nuprc.gov.ng>
2025-07-26 21:55:15 +0100

    Choosing the row layout at runtime

---
+++ CollectionViewDemo/Selectors/ProductDataTemplateSelector.cs
@@ -0,0 +1,20 @@
+﻿using CollectionViewDemo.Models;
+
+namespace CollectionViewDemo.Selectors;
+
+public class ProductDataTemplateSelector : DataTemplateSelector
+{
+    protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+    {
+        if (item is Product product)
+        {
+            if (!product.HasOffer)
+            {
+                Application.Current!.Resources.TryGetValue("ProductStyle", out object? productStyle);
+                return productStyle as DataTemplate ?? new();
+            }
+        }
+
+        return new ();
+    }
+}
