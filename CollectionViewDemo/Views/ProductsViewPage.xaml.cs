86ebb2b66bc470883fc8fc2dd989f9bdf8229821
Abdulmalik Muhammad <abdulmalik.m.m@nuprc.gov.ng>
2025-07-30 07:41:20 +0100

    Detecting Scrolling

--- CollectionViewDemo/Views/ProductsViewPage.xaml.cs
+++ CollectionViewDemo/Views/ProductsViewPage.xaml.cs
@@ -1,4 +1,5 @@
 using CollectionViewDemo.ViewModels;
+using System.Diagnostics;
 
 namespace CollectionViewDemo.Views;
 
@@ -9,4 +10,17 @@ public partial class ProductsViewPage : ContentPage
 		InitializeComponent();
 		BindingContext = viewPageModel;
 	}
+
+    private void CollectionView_Scrolled(object sender, ItemsViewScrolledEventArgs e)
+    {
+		Debug.WriteLine("--------------------------------------------------");
+		Debug.WriteLine($"HorizontalDelta: {e.HorizontalDelta}");
+		Debug.WriteLine($"VerticalDelta: {e.VerticalDelta}");
+		Debug.WriteLine($"HorizontalOffset: {e.HorizontalOffset}");
+		Debug.WriteLine($"VerticalOffset: {e.VerticalOffset}");
+		Debug.WriteLine($"FirstVisibleItemIndex: {e.FirstVisibleItemIndex}");
+		Debug.WriteLine($"CenterItemIndex: {e.CenterItemIndex}");
+		Debug.WriteLine($"LastVisibleItemIndex: {e.LastVisibleItemIndex}");
+        Debug.WriteLine("--------------------------------------------------");
+    }
 }
